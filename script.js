4f241a2307557a3206286210f798bac653249768
João Vitor Madureira <76231431+joaomadureira24@users.noreply.github.com>
2021-10-14 20:30:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let pessoa = {
+    nome: "Josué",
+    idade: 13,
+    cidade: "Taboão da Serra"
+}
+let {nome} = pessoa;
+console.log(nome);
+
+let {idade} = pessoa;
+console.log(idade);
+
+let {cidade} = pessoa;
+console.log(cidade);
